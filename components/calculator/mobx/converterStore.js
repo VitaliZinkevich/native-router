7c0acf436ca1c5d6b0be657c4c1225ee817b2d64
Vitali Zinkevich <vitalizinkevich@gmail.com>
2019-10-12 17:40:31 +0300

    reafctor converter

--- components/calculator/mobx/converterStore.js
+++ components/calculator/mobx/converterStore.js
@@ -9,39 +9,27 @@ import {configure} from "mobx"
 //configure({enforceActions: 'always'})
 //configure({ enforceActions: "observed" })
 
-// инпут добавления валют
-// значения всех инпутов
-// активный инпут 
-
-// добавленные валюты
-// usd eur byn rub
 class ConverterStore  {
 
   inputs = [];
-  activeInputs = [];
   ratesKeys = [];
-  rates = [];
+  rates = {};
   base = '';
   date = '';
   
   makeInputs = (currencies)=>{
     let inputs = [];
-    let baseIndex = null;
-    let activeItem = null;
+    let added = ['USD', 'EUR', 'RUB'];
     for (let i = 0; i < currencies.length; i++ ) {
-      if (currencies[i] === 'USD'){
-        activeItem = {input: 1, select: currencies[i], active: true}
-        inputs.push ( activeItem );
-        baseIndex = i;
+      if (added.findIndex ( (el)=>{return el === currencies[i]})  >=  0){
+        activeItem = {input: this.rates[currencies[i]], select: currencies[i], active: false, added: true};
+        inputs.push (activeItem);
+      } else {
+        inputs.push ({input: this.rates[currencies[i]].toFixed(2), select: currencies[i], active: false, added: false});
       }
-      inputs.push ({input: this.rates[currencies[i]], select: currencies[i], active: false})
-    }
-    inputs.splice (baseIndex, 1);
-    inputs.unshift (activeItem);
     this.inputs = inputs;
-    this.activeInputs = inputs.slice(0, 3);
+    }
   }
-
   getRates = async () => {
     await fetch('https://api.exchangeratesapi.io/latest?base=USD')
       .then(r => r.json())
@@ -49,21 +37,91 @@ class ConverterStore  {
         this.ratesKeys = Object.keys (response.rates);
         this.rates = response.rates;
         this.date = response.date;
-
         this.makeInputs (Object.keys (response.rates)/*JSON.parse(JSON.stringify(this.ratesKeys))*/);
       }))
   };
 
-  changeInput =(text)=>{
-    console.log(text)
+  addNewCurrency = (currency)=>{
+    if(currency){
+      let inpCopy = JSON.parse(JSON.stringify(this.inputs))
+      for (let i = 0; i< inpCopy.length; i++) {
+          if (inpCopy[i].select === currency) {
+            inpCopy[i].added = true;
+            break;
+          }
+      }
+      this.inputs = inpCopy;
+    }
+      
   }
 
+  countRates = (array, activeInput)=>{
+    let activenum = parseInt (activeInput.input ,10)
+    let activeToUsd = activenum / this.rates[activeInput.select]/*за 1 долл*/
+    
+    array.forEach(el=>{
+      if (el.added && el.select !== activeInput.select && activeToUsd) {
+        el.input = (this.rates[el.select] * activeToUsd).toFixed(2);
+      }
+    })
+    return array;
+  }
+
+  changeInput =  ({value, input, picker}) => {
+    let inpCopy = JSON.parse(JSON.stringify(this.inputs))
+    if (picker) {
+      let activeInputIndex = this.inputs.findIndex (el=>{
+        return el.select === value;
+      });
+      let oldActive = this.inputs.findIndex (el=>{
+        return el.select === input.select;
+      });
+      inpCopy[oldActive].added = false;
+      inpCopy[activeInputIndex].added = true;
+      inpCopy[activeInputIndex].input = inpCopy[oldActive].input;
+      let currActive = this.inputs.findIndex (el=>{
+        return el.active === true;
+      });
+      if ( currActive!== -1 ) {
+        inpCopy[currActive].active = false;
+      }
+      inpCopy[activeInputIndex].active = true;
+      inpCopy = this.countRates(inpCopy, inpCopy[activeInputIndex])
+    } else {
+      let currentInputIndex = this.inputs.findIndex (el=>{
+        return el.select === input.select;
+      });
+      
+      inpCopy[currentInputIndex].input = value;
+      
+      let currActive = this.inputs.findIndex (el=>{
+        return el.active === true;
+      });
+      if ( currActive!== -1 ) {
+        inpCopy[currActive].active = false;
+      }
+      inpCopy[currentInputIndex].active = true;
+      inpCopy = this.countRates(inpCopy, inpCopy[currentInputIndex])
+    }
+    this.inputs = inpCopy;
+  }
+
+  delInput =(currency)=>{
+    
+    let inpCopy = JSON.parse(JSON.stringify(this.inputs))
+    for (let i = 0; i<inpCopy.length; i++) {
+      if(inpCopy[i].select === currency){
+        inpCopy[i].added = false;
+        break;
+      }
+    }
+    this.inputs = inpCopy;
+  }
 }
 
 decorate(ConverterStore, {
 
   inputs : observable,
-  activeInputs : observable,
   ratesKeys : observable,
   rates : observable,
   base : observable,
